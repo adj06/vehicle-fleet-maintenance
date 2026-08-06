6299ce9620f4c79d3bce47f7accd4a27782c149c
AJ <51755236+adj06@users.noreply.github.com>
2026-08-06 23:23:37 +0100

    Spring boilerplate code and files

---
+++ src/test/java/com/adesh/vehicle_maintenance/VehicleMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adesh.vehicle_maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
