98eb49a412b46f04aa43dceb03c37a04f3ae69fc
AJ <51755236+adj06@users.noreply.github.com>
2026-09-20 14:07:39 +0100

    validation for faultReport, MaintenanceRecord fields. Mapping db relationships.

--- src/main/java/com/adesh/vehicle_maintenance/repository/MaintenanceRepository.java
+++ src/main/java/com/adesh/vehicle_maintenance/repository/MaintenanceRepository.java
@@ -1,9 +1,10 @@
 package com.adesh.vehicle_maintenance.repository;
 
 import com.adesh.vehicle_maintenance.model.FaultReport;
+import com.adesh.vehicle_maintenance.model.MaintenanceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MaintenanceController extends JpaRepository<FaultReport, Long> {
+public interface MaintenanceRepository extends JpaRepository<MaintenanceRecord, Long> {
 }
